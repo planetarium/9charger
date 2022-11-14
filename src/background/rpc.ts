73558136b06e611f42ba7f77d293e0537665b61a
Akamig <akamig@posteo.net>
2022-11-14 18:41:06 +0900

    Use RPC Selection Logic from 9c-status-monitor
    https://github.com/U-lis/9c-status-monitor/blob/081b0b337f22e5bdef591933eeb6f099107f614d/js/background/rpc.js

---
+++ src/background/rpc.ts
@@ -0,0 +1,25 @@
+export const getRpcNodeList = async (rpcList: string[]) => {
+  const resp = await fetch(
+    "https://download.nine-chronicles.com/9c-launcher-config.json"
+  );
+  if (resp.status === 200) {
+    const r = await resp.json();
+    rpcList = r.RemoteNodeList ?? [];
+    console.log(`${rpcList.length} nodes detected`);
+  } else {
+    // setTimeout(getRpcNodeList, 1000 * 10);
+    console.log("RPC fetch failed. Please try again later.");
+  }
+  await connectRpc(rpcList);
+};
+
+export const connectRpc = async (rpcList: string[]) => {
+  if (rpcList.length === 0) {
+    await getRpcNodeList(rpcList);
+  }
+  const connectedRpc = rpcList[Math.floor(Math.random() * rpcList.length)]
+    .split(",")[0]
+    .concat("/graphql");
+  console.log(`Connect to ${connectedRpc}`);
+  chrome.storage.local.set({ connectedRpc });
+};
