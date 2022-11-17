43b42e739904dabf03c1c93ce532e4c5e93ebe3c
Akamig <akamig@posteo.net>
2022-11-17 17:36:58 +0900

    configure bundling environment

---
+++ src/popup.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import View from "./components/view";
+ReactDOM.render(
+  <React.StrictMode>
+    <View />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
